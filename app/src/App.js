abe2a157d9fbe5a3bc36001b3920531a8c865953
Erica Girges <ericafiorenza@gmail.com>
2021-04-12 13:31:50 -0700

    started styling

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import styled from 'styled-components';
 // components
 import SearchBar from './components/SearchBar';
+
+const Header = styled.header `
+    height: 100px;
+    background-color: #242A2E;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const StyledTitle = styled.h1 `
+    font-family: 'Roboto';
+    font-weight: 700;
+    color: white;
+    font-size: 2.0em;
+`
 
 function App() {
   return (
     <div className="App">
-      <header>
-        <h1>GitHub User Search</h1>
-      </header>
+      <Header>
+        <StyledTitle>GitHub User Search</StyledTitle>
+      </Header>
       <SearchBar />
     </div>
   );
